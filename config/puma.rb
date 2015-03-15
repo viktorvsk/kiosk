32218531b04ef95ff4af278c068807ca5ba7a654
Viktor Vsk <me@viktorvsk.com>
2015-03-15 20:55:11 +0200

    Fix puma deploy

--- config/puma.rb
+++ config/puma.rb
@@ -8,7 +8,7 @@ rackup "#{deploy_to}/current/config.ru"
 
 directory       "#{deploy_to}/current"
 pidfile         "#{deploy_to}/shared/tmp/pids/puma.pid"
-state_path      "#{deploy_to}/shared/tmp/pids/puma.state"
+state_path      "#{deploy_to}/shared/tmp/sockets/puma.state"
 stdout_redirect "#{deploy_to}/shared/log/puma.stdout.log", "#{deploy_to}/shared/log/puma.stderr.log"
 bind            "unix://#{deploy_to}/shared/tmp/sockets/kiosk.sock"
 activate_control_app "unix://#{deploy_to}/shared/tmp/sockets/pumactl.sock"
