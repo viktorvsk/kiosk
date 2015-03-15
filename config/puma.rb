a67ee091bbd218cbe4eb3be735c7cc1030466067
Viktor Vsk <me@viktorvsk.com>
2015-03-15 20:33:24 +0200

    Fix puma deploy

--- config/puma.rb
+++ config/puma.rb
@@ -1,13 +1,13 @@
 deploy_to = '/home/vvsk/kiosk'
 
 environment    'production'
-daemonize       false
+daemonize       true
 threads 0, 16
 rackup "#{deploy_to}/current/config.ru"
-
 
 directory       deploy_to
 pidfile         "#{deploy_to}/shared/tmp/pids/puma.pid"
 state_path      "#{deploy_to}/shared/tmp/pids/puma.state"
 stdout_redirect "#{deploy_to}/shared/log/puma.stdout.log", "#{deploy_to}/shared/log/puma.stderr.log"
 bind            "unix://#{deploy_to}/shared/tmp/sockets/kiosk.sock"
+activate_control_app 'unix:///var/run/pumactl.sock'
