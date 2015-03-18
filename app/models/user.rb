22307d8b9c291c5ef2de67b7fc0e465b5fb1cbd1
Viktor Vsk <me@viktorvsk.com>
2015-03-18 23:33:16 +0200

    User roles with State model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,31 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  ROLES = %w(admin customer)
+  scope :admins, -> { joins(:role).where(states: { name: 'admin' }) }
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
+  has_one :role, as: :stateable, class_name: State
+
+  ROLES.each do |role_name|
+    define_method "#{role_name}?" do
+      role.present? && role.name == role_name.to_s
+    end
+  end
+
+  def role_name
+    role.name
+  end
+
+  def customer?
+    role.nil? or role.name == 'customer'
+  end
+
+  def promote_to(role_name)
+    fail ArgumentError, "Invalid role. Use one of: #{ROLES.join(', ')}" unless role_name.to_s.in?( ROLES )
+    if role.present?
+      role.update(name: role_name.to_s) unless role.name == role_name.to_s
+    else
+      create_role(name: role_name)
+    end
+    role_name
+  end
 end
