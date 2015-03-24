dd1dcf68497c0bbf23a0598e6b973fd35445c7c1
Viktor Vsk <me@viktorvsk.com>
2015-03-24 11:49:33 +0200

    Vendor::Product state at loading

--- app/models/vendor/product.rb
+++ app/models/vendor/product.rb
@@ -8,10 +8,33 @@ class Vendor::Product < ActiveRecord::Base
   belongs_to :product, class_name: Catalog::Product, foreign_key: :catalog_product_id
   belongs_to :merchant, class_name: Vendor::Merchant, foreign_key: :vendor_merchant_id
   belongs_to :catalog_category, class_name: Catalog::Category
-
+  has_one :state, as: :stateable
+  scope :active,      ->{ joins('LEFT JOIN states ON states.stateable_id = vendor_products.id').where('states.stateable_id IS NULL') }
+  scope :not_active,  ->{ joins(:state).where(states: { name: 'inactive' }) }
+  scope :bound,       ->{ where('vendor_products.catalog_product_id IS NOT NULL') }
+  scope :unbound,     ->{ where('vendor_products.catalog_product_id IS NULL') }
+  scope :rrc,         ->{ where(is_rrc: true) }
   class << self
     def to_csv
       all.map(&:to_csv).join("\n");
+    end
+
+    def activate
+      ids = all.joins(:state).uniq.pluck(:id)
+      State.where(stateable_id: ids, stateable_type: 'Vendor::Product').delete_all
+    end
+
+    def deactivate
+      to_create = all.active.pluck(:id).map do |id|
+        {
+          stateable_id: id,
+          stateable_type: 'Vendor::Product',
+          name: 'inactive'
+        }
+      end
+      State.transaction do
+        State.create( to_create )
+      end
     end
 
     def unbind
@@ -21,10 +44,30 @@ class Vendor::Product < ActiveRecord::Base
     def bind_to( catalog_product )
       all.update_all( product: catalog_product )
     end
+
+    def select_rrc
+      all.rrc.first
+    end
   end
 
   def bind_to( catalog_product )
     update!( product: catalog_product )
+  end
+
+  def activate
+    create_state(name: 'inactive')
+  end
+
+  def deactivate
+    state ? state.destroy : true
+  end
+
+  def active?
+    !state.present?
+  end
+
+  def bound?
+    product.present?
   end
 
   def unbind
@@ -50,3 +93,23 @@ class Vendor::Product < ActiveRecord::Base
     ]
   end
 end
+
+
+
+
+
+
+a1 = [2,3,4,5]
+a2 = [2,3,4,5,6,7,8,9,10]
+
+
+
+
+
+
+
+
+
+
+
+
--- app/services/vendor/pricelist.rb
+++ app/services/vendor/pricelist.rb
@@ -8,24 +8,42 @@ module Vendor
     CSV_COLUMNS       = %w(articul name price in_stock vendor_merchant_id catalog_product_id created_at updated_at info is_rrc).join(',').freeze
 
     def initialize(merchant, file)
-      @file = file
-      @merchant = merchant
-      @settings = @merchant.to_activepricelist
+      @file         = file
+      @merchant     = merchant
+      @settings     = @merchant.to_activepricelist
       @parser_class = @merchant.parser_class.presence || 'Default'
     end
 
     def import!
-      upload_pricelist
-      parse_pricelist
       GC.enable
       GC.start
+      upload_pricelist
+      parse_pricelist
+      GC.start
+      @products_names    = @merchant.products.pluck('vendor_products.name')
+      @products_articuls = @merchant.products.pluck('vendor_products.articul')
+      delete_not_in_pricelist
       batch_create_or_update
-      GC.enable
       GC.start
       true
     end
 
   private
+
+    def delete_not_in_pricelist
+      @to_delete = @products_articuls - @products.map{ |p| p['articul'] }
+
+      @merchant.
+        products.
+        unbound.
+        where(vendor_products: { articul: @to_delete }).
+        delete_all
+      @merchant.
+        products.
+        bound.
+        where(vendor_products: { articul: @to_delete }).
+        deactivate
+    end
 
     def upload_pricelist
        File.open(@merchant.pricelist_path, 'wb'){ |f| f.puts @file.read }
@@ -38,13 +56,15 @@ module Vendor
     end
 
     def batch_create_or_update
-      to_create, to_update = from_hash_to_update_and_to_create
-      batch_update( to_update )
-      batch_create( to_create )
+      from_hash_to_update
+      from_hash_to_create
+      batch_update( @to_update )
+      GC.start
+      batch_create( @to_create )
     end
 
     def batch_update( products )
-      sql = products.map do |p|
+      sql = products.compact.map do |p|
         attributes_to_update = []
         p.attributes.keys.select{ |q| q.in?(UPDATE_ATTRIBUTES) }.each do |att|
           next unless p[att].to_s.present?
@@ -71,32 +91,30 @@ module Vendor
       end
     end
 
-    def from_hash_to_update_and_to_create
-      to_create, to_update = [], []
-
-      articuls            = @products.map{ |p| p['articul'] }
-      products_names      = ::Vendor::Product.pluck(:name)
-      products_articuls   = ::Vendor::Product.pluck(:articul)
-      to_update           = @merchant.products.where(articul: articuls).all
-      to_update_articuls  = to_update.pluck(:articul)
-
-      to_update.map do |product|
+    def from_hash_to_update
+      articuls             = @products.map{ |p| p['articul'] }
+      @to_update           = @merchant.products.where(articul: articuls).all
+      @to_update_articuls  = @to_update.pluck(:articul)
+      @to_update = @to_update.map do |product|
         new_product = @products.detect{ |p| p['articul'] == product.articul }
         next if product['price'].to_i == new_product['price'].to_i
         new_product_attributes = new_product.slice( *UPDATE_ATTRIBUTES )
         new_product_attributes.each do |k, v|
           product.send("#{k}=", v)
         end
-      end
 
-      to_create = @products.reject{ |p| p['articul'].in?( to_update_articuls ) }
-      to_create.each{ |p| p['vendor_merchant_id'] = @merchant.id  }
-      to_create.map!{ |p| ::Vendor::Product.new(p.slice( *REAL_ATTRIBUTES )) }
+        product
+      end
+    end
+
+    def from_hash_to_create
+      @to_create = @products.reject{ |p| p['articul'].in?( @to_update_articuls ) }
+      @to_create.each{ |p| p['vendor_merchant_id'] = @merchant.id  }
+      @to_create.map!{ |p| ::Vendor::Product.new(p.slice( *REAL_ATTRIBUTES )) }
       # @TODO manual validations
-      to_create.uniq!{ |p| p.name }
-      to_create.uniq!{ |p| p.articul }
-      to_create.reject!{ |p| p.articul.blank? or p.name.in?(products_names) or p.articul.in?(products_articuls) }
-      return to_create, to_update
+      @to_create.uniq!{ |p| p.name }
+      @to_create.uniq!{ |p| p.articul }
+      @to_create.reject!{ |p| p.articul.blank? or p.name.in?(@products_names) or p.articul.in?(@products_articuls) }
     end
 
   end
