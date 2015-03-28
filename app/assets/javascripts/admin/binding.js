8211b308d72b52c5095d3489ebf1b67ea46a5187
Viktor Vsk <me@viktorvsk.com>
2015-03-29 00:10:20 +0200

    Imrove binding js/css

--- app/assets/javascripts/admin/binding.js
+++ app/assets/javascripts/admin/binding.js
@@ -1,72 +1,137 @@
-Binding = {
-  init: function () {
-    $(Binding.selector.vendorProduct).draggable({ revert: 'invalid' });
+/*jslint browser: true */
+(function () {
+  function Binding() {
+    var that = this;
 
-    $(Binding.selector.product).droppable({
-      hoverClass: 'test',
-      cursorAt: { top: -5, left: -5 },
-      disabled: false,
-      drop: Binding.onBind
-    });
+    this.vendorProduct        = '[data-draggable="vendorProduct"]';
+    this.product              = '[data-droppable="product"]';
+    this.vendorProductsTable  = '[data-droppable="vendorProductsTable"]';
+    this.productsTable        = '[data-catalog-products]';
+    this.dataVendorProductId  = 'vendor-product-id';
+    this.dataProductId        =  'product-id';
+    this.bindClass            = 'bind-to-product';
+    this.unbindClass          = 'unbind-vendor-product';
+    this.spinnerClass         = 'glyphicon glyphicon-refresh fa-spin';
 
-    $(Binding.selector.vendorProductsTable).droppable({
-      hoverClass: 'test',
-      drop: Binding.onUnbind
-    })
-  },
-  bind: function (url) {
-    $.post(url);
-  },
-  unbind: function(url) {
-    $.ajax({
-      url: url,
-      type: 'DELETE'
-    });
-  },
-  selector: {
-    vendorProduct: '[data-draggable="vendorProduct"]',
-    product: '[data-droppable="product"]',
-    vendorProductsTable: '[data-droppable="vendorProductsTable"]'
-  },
-  data: {
-    vendorProductId: 'vendor-product-id',
-    productId: 'product-id'
-  },
-  url: {
-    binding: function (product, vendorProduct) {
+    this.urlForBind = function (product, vendorProduct) {
       return ['/admin', 'binding', product, vendorProduct].join('/');
-    },
-    unbinding: function (vendorProduct) {
+    };
+
+    this.urlForUnbind = function (vendorProduct) {
       return ['/admin', 'binding', vendorProduct].join('/');
-    }
-  },
-  onUnbind: function (event, ui) {
-    var $vendorProductNode  = $(ui.draggable),
-        vendorProductId     = $vendorProductNode.data(Binding.data.vendorProductId);;
-    Binding.unbind(Binding.url.unbinding(vendorProductId));
-    $vendorProductNode.remove();
-    $("form").submit()
-    Binding.init();
+    };
 
-  },
-  onBind: function ( event, ui ) {
-    var $productNode        = $(this),
+    this.urlForRecountProduct = function (id) {
+      return ['/admin', 'products', id, 'recount'].join('/');
+    };
+
+    this.urlForUpdateProduct = function (id) {
+      return ['/admin', 'products', id].join('/');
+    };
+
+    // this.findProduct = function (id) {
+    //   var selector = ['[data-product-id="', id, '"]'].join('');
+    //   return $(selector);
+    // };
+
+    this.updateProduct = function ($product) {
+      var spinner = $('<i/>').addClass(that.spinnerClass),
+        id = $product.data(that.dataProductId),
+        url = that.urlForUpdateProduct(id);
+      $product
+        .prepend(spinner)
+        .droppable('disable');
+      $.get(url, function (data) {
+        $product.replaceWith(data);
+        that.init();
+      });
+
+    };
+
+    this.bindFromProduct = function ($vendorProduct) {
+      return $vendorProduct.parent().parent().data('droppable') === 'product';
+    };
+
+    this.init = function () {
+      $(that.vendorProduct).draggable(that.draggableConfig);
+      $(that.product).droppable(that.droppableProductConfig);
+      $(that.vendorProductsTable).droppable(that.droppableVendorProductConfig);
+    };
+
+    this.bind = function (product, vendorProduct) {
+      var url = that.urlForBind(product, vendorProduct);
+      $.post(url);
+    };
+
+    this.productFor = function ($vendorProduct) {
+      return $vendorProduct.parent().parent();
+    };
+
+    this.unbind = function (vendorProduct) {
+      var url = that.urlForUnbind(vendorProduct);
+      $.ajax({
+        url: url,
+        type: 'DELETE'
+      });
+    };
+
+    this.recountProduct = function ($product) {
+      var id = $product.data(that.dataProductId),
+        url = that.urlForRecountProduct(id);
+      $.post(url, function (data) {
+        that.updateProduct($product);
+        console.log(data);
+      });
+    };
+
+    this.onUnbind = function (event, ui) {
+      var $vendorProductNode = $(ui.draggable),
+        vendorProductId = $vendorProductNode.data(that.dataVendorProductId),
+        $product = that.productFor($vendorProductNode);
+      that.unbind(vendorProductId);
+      $vendorProductNode
+        .css({ top: 0, left: 0, right: 0, bottom: 0 })
+        .prependTo($(that.vendorProductsTable));
+      that.updateProduct($product);
+      that.init();
+    };
+
+    this.onBind = function (event, ui) {
+      var $productNode      = $(this),
+        $oldProduct         = null,
         $vendorProductNode  = $(ui.draggable),
-        productId           = $productNode.data(Binding.data.productId),
-        vendorProductId     = $vendorProductNode.data(Binding.data.vendorProductId);
+        productId           = $productNode.data(that.dataProductId),
+        vendorProductId     = $vendorProductNode.data(that.dataVendorProductId);
 
-    // $vendorProduct = $('<li>')
-    //                   .text($vendorProductNode.text())
-    //                   .attr('data-draggable', 'vendorProduct')
-    //                   .attr('data-vendor-product-id', vendorProductId);
-    // $productNode
-    //   .find('ul')
-    //     .append($vendorProduct);
+      that.bind(productId, vendorProductId);
+      if (that.bindFromProduct($vendorProductNode)) {
+        $oldProduct = that.productFor($vendorProductNode);
+      }
+      $vendorProductNode.remove();
+      that.updateProduct($productNode);
+      that.recountProduct($oldProduct);
 
-    Binding.bind(Binding.url.binding(productId, vendorProductId));
-    $vendorProductNode.remove();
-    $("form").submit()
-    Binding.init();
+      that.init();
+    };
+
+    this.draggableConfig = {
+      revert: 'invalid',
+      cancel: 'span'
+    };
+
+    this.droppableProductConfig = {
+      hoverClass: that.bindClass,
+      cursorAt: { top: -5, left: -5 },
+      drop: that.onBind
+    };
+
+    this.droppableVendorProductConfig = {
+      hoverClass: that.unbindClass,
+      drop: that.onUnbind,
+      accept: that.productsTable + ' ' + 'li'
+    };
 
   }
-};
+
+  window.Binding = new Binding();
+}());
