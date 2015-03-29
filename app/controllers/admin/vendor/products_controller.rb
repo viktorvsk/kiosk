80a588e5150b387be6d910778abeb7d2ae04c586
Viktor Vsk <me@viktorvsk.com>
2015-03-30 00:47:07 +0300

    Fix activation status + recount

--- app/controllers/admin/vendor/products_controller.rb
+++ app/controllers/admin/vendor/products_controller.rb
@@ -17,8 +17,9 @@ class Admin::Vendor::ProductsController < Admin::BaseController
   end
 
   def toggle_activation
-    @product = ::Vendor::Product.find(params[:id])
-    @product.active? ? @product.deactivate : @product.activate
+    @vendor_product = ::Vendor::Product.find(params[:id])
+    @vendor_product.active? ? @vendor_product.deactivate : @vendor_product.activate
+    @vendor_product.product.recount if @vendor_product.product
     # render text: @product.state.try(:name)
   end
 
