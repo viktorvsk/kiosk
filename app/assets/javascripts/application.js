5c00939ad5048acf3ff3b61fb698f0f45d72f9e5
Viktor Vsk <me@viktorvsk.com>
2015-03-29 02:24:31 +0200

    Get bootstrap back

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-// require bootstrap
+//= require bootstrap
 //= require jquery-ui/ui/core
 //= require jquery-ui/ui/widget
 //= require jquery-ui/ui/mouse
