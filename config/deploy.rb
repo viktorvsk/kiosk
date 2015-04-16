d7bf24a0fbbbc924c12f20ff3925523fa4609555
Viktor Vsk <me@viktorvsk.com>
2015-04-17 01:34:38 +0300

    Trying to fix resque deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -89,7 +89,7 @@ task :deploy => :environment do
 
     to :launch do
       queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
-      invoke :'resque:restart_workers'
+      Rake::Task['resque:restart_workers'].invoke
       invoke :'puma:phased_restart'
     end
   end
