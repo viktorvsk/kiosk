677752f38872a8426191658b5587d9b6326d35ae
Viktor Vsk <me@viktorvsk.com>
2015-04-26 12:42:27 +0300

    Add ErcMarketplace

--- app/marketplaces/erc_marketplace.rb
+++ app/marketplaces/erc_marketplace.rb
@@ -1,45 +1,40 @@
 class ErcMarketplace < BasicMarketplace
-  HOST = /brain/
-
+  HOST = /erc/
 
   private
 
   def search_query
-    "http://brain.com.ua/search/#{@query}"
+    "http://www.erc.ua/ru/search/#{@query}"
   end
 
   def search_found_selector
-    '.photo_block'
+    '.ware'
   end
 
   def search_results_mapper(product)
-    res = {
-      name: product.at_css('.photo_block_headline a').text.strip,
-      url: URI.join('http://brain.com.ua', product.at_css('.photo_block_headline a')['href']).to_s,
-      image: URI.join('http://brain.com.ua', product.at_css('.photo img')['src']).to_s
-    }
-    res[:price] = if (node = product.at_css('.prise')) && node.present?
-      node.text.to_i
-    else
-      res[:do_not_show] = false
-      'Нет в наличии'
-    end
+    res = {}
+    res[:image] = nil
+    res[:name]  = product.at_css('a').text.strip
+    res[:url]   = URI.join('http://erc.ua', product.at_css('a')['href']).to_s
+    res[:price] = product.parent.at_css(".price").text.strip.split(',')[0].scan(/\d+/).join rescue 'Нет в наличии'
     res
+  rescue
+    nil
   end
 
   def product_page_scraper(page)
-    properties = page.css('.product_specifications tr').map do |row|
+    res = {}
+    properties = page.css('.vido_frame_properties_table tr').map do |row|
       {
-        row.at_css('th').text.strip => row.at_css('td').text.strip
-      }
+        row.css('td')[0].text.strip => row.css('td')[1].text.strip
+      } rescue nil
     end
-    {
-      name: page.at_css('h1').text.strip,
-      description: page.at_css('.description').inner_html.strip,
-      images: page.css('.thumbs_list a').map { |a| URI.join('http://brain.com.ua', a['href']).to_s },
-      properties: properties,
-      category: page.css('.crumbs a')[1].text.strip,
-      url: @query
-    }
+    res[:name]        = page.at_css('#cpContent_divTitle').inner_html.split('<br>')[0].gsub(/^.+: /, '') rescue page.at_css('#cpContent_divTitle').try(:text)
+    res[:description] = page.at_css('#tab_properties_desc').inner_html.strip rescue ''
+    res[:images]      = page.css('.vido_frame_big_gallery_container img').map { |i| URI.join('http://erc.ua', i['src']).to_s rescue nil }.compact rescue []
+    res[:properties]  = properties.select(&:present?)
+    res[:url]         = @query
+    # res[:category]    = page.at_css('.cpContent_tbVnd_pnVnd_tvVnd_2.cpContent_tbVnd_pnVnd_tvVnd_4').text.strip
+    res
   end
 end
