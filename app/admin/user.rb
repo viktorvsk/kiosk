7f8ef3f26a0544da5d3edcebb3b4a130efc6f7a9
Viktor Vsk <me@viktorvsk.com>
2015-04-30 11:11:24 +0300

    Some

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation, :name, :phone
   actions :all, except: [:show]
   menu priority: 1, parent: "CRM"
   filter :email
@@ -10,8 +10,10 @@ ActiveAdmin.register User do
   end
 
   form do |f|
-    f.inputs "Admin Details" do
+    f.inputs "Детали" do
       f.input :email
+      f.input :name
+      f.input :phone
       f.input :password
       f.input :password_confirmation
     end
