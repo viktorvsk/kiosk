58938046e952c94c677b4551a6afd2f2dce5d7c8
Viktor Vsk <me@viktorvsk.com>
2015-04-30 13:10:09 +0300

    Add JS and CSS Settings to confs

--- config/initializers/confs.rb
+++ config/initializers/confs.rb
@@ -1,15 +1,17 @@
 default_settings = {
-  site_name: 'Киоск',
-  'ck.logo' => 'Логотип',
-  'ck.footer1' => '1',
-  'ck.footer2' => '2',
-  'ck.footer3' => '3',
-  'ck.vk_group' => '3',
-  'ck.phones' => '+380931234567',
-  'ck.phones_and_mode' => 'выходные',
-  'ck.homepage_seo' => 'Интернет-магазин ...',
-  'ck.mode' => '<h1>Режим работы</h1>',
-  'seo_template_product' => '{{название}} за {{цена}} грн. в Харькове'
+  'site_name'             => 'Киоск',
+  'seo_template_product'  => '{{название}} за {{цена}} грн. в Харькове',
+  'txt.js'                => '',
+  'txt.css'               => '',
+  'ck.logo'               => 'Логотип',
+  'ck.footer1'            => '1',
+  'ck.footer2'            => '2',
+  'ck.footer3'            => '3',
+  'ck.vk_group'           => '3',
+  'ck.phones'             => '+380931234567',
+  'ck.phones_and_mode'    => 'выходные',
+  'ck.homepage_seo'       => 'Интернет-магазин ...',
+  'ck.mode'               => '<h1>Режим работы</h1>'
 }
 
 default_settings.each do |k, v|
