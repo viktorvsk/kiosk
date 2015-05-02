8d8f2b72a7464273ea16e540847993276a817f6b
Viktor Vsk <me@viktorvsk.com>
2015-05-02 12:31:43 +0300

    Fix current_order setup for devise and admin controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_current_order
 
 private
   def authorize_admin!
@@ -15,5 +16,29 @@ private
 
   def authenticate_admin_user!
     redirect_to new_user_session_path unless current_user.try(:is_admin?)
+  end
+
+  def set_current_order
+    return if request.path =~ /^\/admin/
+    current_user ? set_order_for_user : set_order_for_guest
+  end
+
+  def set_order_for_user
+    @current_order = Order.in_cart.where(id: session[:order_id], user: nil).first ||
+      current_user.current_order || current_user.create_current_order(phone: current_user.phone, name: current_user.name)
+    session[:order_id] = @current_order.id
+    if !@current_order.user
+      current_user.current_order.destroy
+      @current_order.update user: current_user, name: current_user.name, phone: current_user.phone
+    end
+  end
+
+  def set_order_for_guest
+    if session[:order_id]
+      @current_order = Order.in_cart.where(id: session[:order_id]).first || Order.create
+    else
+      @current_order = Order.create!
+      session[:order_id] = @current_order.id
+    end
   end
 end
--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,29 +1,3 @@
 class CatalogController < ApplicationController
-  before_action :set_current_order
-
-  private
-
-  def set_current_order
-    current_user ? set_order_for_user : set_order_for_guest
-  end
-
-  def set_order_for_user
-    @current_order = Order.in_cart.where(id: session[:order_id], user: nil).first ||
-      current_user.current_order || current_user.create_current_order(phone: current_user.phone, name: current_user.name)
-    session[:order_id] = @current_order.id
-    if !@current_order.user
-      current_user.current_order.destroy
-      @current_order.update user: current_user, name: current_user.name, phone: current_user.phone
-    end
-  end
-
-  def set_order_for_guest
-    if session[:order_id]
-      @current_order = Order.in_cart.where(id: session[:order_id]).first || Order.create
-    else
-      @current_order = Order.create!
-      session[:order_id] = @current_order.id
-    end
-  end
 
 end
