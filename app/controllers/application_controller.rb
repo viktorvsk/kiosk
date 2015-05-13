bc951729c405dfd0c4a094388eebdc494586fa18
Viktor Vsk <me@viktorvsk.com>
2015-05-13 22:03:42 +0300

    Fix guest current_order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,23 +24,19 @@ private
   end
 
   def set_order_for_user
-    session_order = Order.in_cart.where(id: session[:order_id], user: nil).first
+    session_order = Order.where(state: 'in_cart').where(id: session[:order_id], user: nil).first
     @current_order = session_order ||
       current_user.current_order ||
       current_user.create_current_order(phone: Catalog.strip_phone(current_user.phone), name: current_user.name)
     session[:order_id] = @current_order.id
     if !@current_order.user
       current_user.current_order.try(:destroy)
-      @current_order.update user: current_user, name: current_user.name, phone: current_user.phone
+      @current_order.update user: current_user, name: current_user.name, phone: Catalog.strip_phone(current_user.phone)
     end
   end
 
   def set_order_for_guest
-    if session[:order_id]
-      @current_order = Order.in_cart.where(id: session[:order_id]).first || Order.create
-    else
-      @current_order = Order.create!
-      session[:order_id] = @current_order.id
-    end
+    @current_order = Order.where(state: 'in_cart').where(id: session[:order_id]).first || Order.create!
+    session[:order_id] = @current_order.id
   end
 end
