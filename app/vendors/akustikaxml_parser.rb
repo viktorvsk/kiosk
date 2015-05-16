00f98299f0593cfb0b66efa56dcbbd9526dd114c
Viktor Vsk <me@viktorvsk.com>
2015-05-17 02:16:27 +0300

    Fix akustika

--- app/vendors/akustikaxml_parser.rb
+++ app/vendors/akustikaxml_parser.rb
@@ -7,4 +7,20 @@ class AkustikaxmlParser < ::ActivePricelist::Base
     Typhoeus.get("http://xmlex.kin.dp.ua/21ev/priceA00002851.xml").body.force_encoding("UTF-8")
   end
 
+  def transform
+    @currency_order.each do |curr|
+      if @product[curr].to_f.ceil > 0
+        @product['price'] = @product[curr].to_f.ceil
+        if curr == 'rrc'
+          @product['is_rrc'] = true
+        else
+          @product['price'] = (@product['price'].to_f * @rates[curr] * (100 - @discount.to_i) / 100).ceil
+        end
+        break
+      end
+    end
+    @product['price'] += @product['delivery_tax'].to_i if !@product['is_rrc'] && (@product['price'].to_f.ceil > 0)
+    @product['in_stock'] = !@not_in_stock.any? { |sign| @product['not_in_stock'] =~ /#{sign}/ }
+  end
+
 end
