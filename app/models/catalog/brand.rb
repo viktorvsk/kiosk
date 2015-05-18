57df83c700ebddf78e5207edc156632840be1068
Viktor Vsk <me@viktorvsk.com>
2015-05-19 01:11:47 +0300

    Fix brand name uniqueness on model side

--- app/models/catalog/brand.rb
+++ app/models/catalog/brand.rb
@@ -2,6 +2,7 @@ class Catalog::Brand < ActiveRecord::Base
 
   store_accessor :info, :description, :slug
   validates :name, :slug, :description, presence: true
+  validates :name, uniqueness: { case_insensitive: true }
   has_one :seo, as: :seoable, dependent: :destroy
   has_one :image, as: :imageable, dependent: :destroy
   has_many :products, class_name: Catalog::Product, foreign_key: :catalog_brand_id
