a30bac1918ce7f6fe0a782aab582555b8282585c
Viktor Vsk <me@viktorvsk.com>
2015-06-07 17:49:19 +0300

    SEO for static pages

--- app/admin/markup.rb
+++ app/admin/markup.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Markup do
   menu parent: 'Настройки'
   actions :all, except: [:show]
-  permit_params :name, :active, :slug, :body, :markup_type, :position
+  permit_params :name, :active, :slug, :body, :markup_type, :position, seo_attributes: [:title, :description, :keywords]
   scope "Активные", :active
   Markup::TYPES.each do |m|
     scope I18n.t("activerecord.attributes.markup.types.#{m.pluralize}"), m.pluralize
@@ -30,6 +30,15 @@ ActiveAdmin.register Markup do
       f.input :position
       f.input :markup_type, as: :select, collection: Markup::TYPES.map{ |m| [t("activerecord.attributes.markup.types.#{m}"), m] }, include_blank: false
       f.input :body, as: :ckeditor, input_html: { ckeditor: { language: :ru } }
+    end
+
+    inputs "SEO" do
+      f.object.build_seo unless f.object.seo
+      f.has_many :seo, heading: 'Мета теги', allow_destroy: false, new_record: false do |i|
+        i.input :title
+        i.input :description
+        i.input :keywords
+      end
     end
     actions
   end
--- app/models/markup.rb
+++ app/models/markup.rb
@@ -1,9 +1,11 @@
 class Markup < ActiveRecord::Base
   include Slugable
+  has_one :seo, as: :seoable, dependent: :destroy
   TYPES = %w(page article help slide)
   TYPES.each do |t|
     scope t.pluralize.to_sym, -> { where(markup_type: t) }
   end
   scope :pages_and_articles, -> { where("markup_type IN ('page','article')") }
   scope :active, -> { where(active: true) }
+  accepts_nested_attributes_for :seo
 end
