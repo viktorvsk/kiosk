5b772359738f21d9b38dea0b7b624d3e6b2697b4
weare138 <pecherin.dev@gmail.com>
2016-02-23 01:02:44 +0200

    fix bug with null count

--- app/vendors/timautoxml_parser.rb
+++ app/vendors/timautoxml_parser.rb
@@ -29,13 +29,22 @@ class TimautoxmlParser < ::ActivePricelist::Base
     #   node.tap{ |x| x.content = available_state }
     # end
   end
-
+  
   def transform
     @currency_order.each do |curr|
       if @product[curr].to_f.ceil > 0
-        @product['price'] = (@product['curr'].to_f * @rates[curr].to_f * (100 - @discount.to_i / 100).ceil)
+        @product['price'] = @product[curr].to_f
+        if curr == 'rrc'
+          @product['is_rrc'] = true
+        else
+          @product['is_rrc'] = false
+          @product['price'] = (@product['price'].to_f * @rates[curr].to_f * (100 - @discount.to_i) / 100).ceil
+        end
+        break
       end
     end
     @product['price'] = @product['price'].to_f.ceil
+    @product['price'] += @product['delivery_tax'].to_i if !@product['is_rrc'] && (@product['price'].to_f.ceil > 0)
+    @product['in_stock'] = !@not_in_stock.any? { |sign| @product['not_in_stock'] =~ /#{sign}/ }
   end
 end
