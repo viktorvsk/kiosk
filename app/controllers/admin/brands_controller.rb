d4e27f6965bd061c112dfa49a64338b2418143eb
weare138 <pecherin.dev@gmail.com>
2016-02-29 19:04:31 +0200

    transfer brands from AA

--- app/controllers/admin/brands_controller.rb
+++ app/controllers/admin/brands_controller.rb
@@ -2,10 +2,9 @@ class Admin::BrandsController < Admin::BaseController
   before_action :check_content_manager_permissions
   before_action :set_brand, only: [:edit, :update, :destroy]
 
-  # GET /admin/brands
   def index
     @q = Catalog::Brand.ransack
-    @brands = Catalog::Brand.page(params[:page])
+    @brands = Catalog::Brand.order(created_at: :desc).page(params[:page])
   end
 
   def search
@@ -15,50 +14,37 @@ class Admin::BrandsController < Admin::BaseController
     render :index
   end
 
-  # GET /admin/brands/new
   def new
     @brand = ::Catalog::Brand.new
     @brand.build_seo
     @brand.build_image
   end
 
-  # GET /admin/brands/1/edit
   def edit
     @brand.build_seo unless @brand.seo
     @brand.build_image unless @brand.image
   end
 
-  # POST /admin/brands
   def create
     @brand = ::Catalog::Brand.new(brand_params)
-
-    respond_to do |format|
-      if @brand.save
-        format.html { redirect_to admin_brands_url, notice: 'Бренд создан.' }
-      else
-        format.html { render :new }
-      end
+    if @brand.save
+       redirect_to admin_brands_url
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /admin/brands/1
   def update
-    respond_to do |format|
-      if @brand.update(brand_params)
-        format.html { redirect_to admin_brands_url, notice: 'Бренд обновлен.' }
-      else
-        format.html { render :edit }
-      end
+    if @brand.update(brand_params)
+      redirect_to admin_brands_url
+    else
+      render :edit
     end
   end
 
-  # DELETE /admin/brands/1
   def destroy
     @brand.destroy
-    respond_to do |format|
-      format.html { redirect_to admin_brands_url, notice: 'Бренд удален.' }
-      format.json { head :no_content }
-    end
+    redirect_to admin_brands_url
   end
 
   private
