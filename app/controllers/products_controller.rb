8cfc0780fef867da6afcf32f6dd5f882617adeba
Viktor Vsk <me@viktorvsk.com>
2016-03-15 15:21:15 +0200

    Refactory seo_meta_titles using CategoryPresenter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,9 @@ class ProductsController < CatalogController
     @newest = top_products.select{ |p| p.newest == '1' }
     @homepage = top_products.select{ |p| p.homepage == '1' }
     @hit = top_products.select{ |p| p.hit == '1' }
+    @meta_title = Conf['kiosk_meta_title']
+    @meta_keywords = Conf['kiosk_meta_description']
+    @meta_description = Conf['kiosk_meta_keywords']
   end
 
   def search
@@ -46,6 +49,9 @@ class ProductsController < CatalogController
         @newest = top_products.select{ |p| p.newest == '1' }
         @homepage = top_products.select{ |p| p.homepage == '1' }
         @hit = top_products.select{ |p| p.hit == '1' }
+        @meta_title = presents(@product).seo_meta_title(Conf['seo_template_product'])
+        @meta_keywords = @product.seo.try(:keywords)
+        @meta_description = @product.seo.try(:description)
       end
       format.js
     end
---
+++ app/presenters/catalog/category_presenter.rb
@@ -0,0 +1,17 @@
+class Catalog::CategoryPresenter < SimpleDelegator
+
+  def seo_meta_title(template)
+    if seo.present? && seo.title.present?
+      seo.title
+    else
+      seo_mapper = { 'имя-мн' => name, 'имя-ед' => s_name }
+      seo_regexp = /\{\{(#{seo_mapper.keys.join('|')})\}\}/
+      template.gsub(seo_regexp) { seo_mapper[$1] }
+    end
+  end
+
+  def model
+    __getobj__
+  end
+
+end
