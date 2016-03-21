604a45f273c31b70f7a36362ee795781497cea41
Viktor Vsk <me@viktorvsk.com>
2016-03-22 01:33:24 +0200

    Refactor and optimize

--- app/services/vendor/pricelist.rb
+++ app/services/vendor/pricelist.rb
@@ -2,170 +2,126 @@ require 'csv'
 
 module Vendor
   class Pricelist
-    @queue            = :pricelist_import
-    UPDATE_ATTRIBUTES = %w(uah usd eur rrc in_stock in_stock_kharkov in_stock_kiev price is_rrc)
-    REAL_ATTRIBUTES   = %w(uah usd eur rrc in_stock_kharkov in_stock_kiev model brand category warranty name price in_stock is_rrc articul vendor_merchant_id)
-    CSV_COLUMNS       = %w(articul name price in_stock vendor_merchant_id catalog_product_id created_at updated_at info is_rrc).join(',').freeze
+    @queue = :pricelist_import
+    REAL_ATTRIBUTES = %w(uah usd eur rrc in_stock_kharkov in_stock_kiev model brand category warranty name price in_stock is_rrc articul vendor_merchant_id).map(&:freeze).freeze
+    CSV_COLUMNS = %w(articul name price in_stock vendor_merchant_id catalog_product_id created_at updated_at info is_rrc).join(',').freeze
 
     class << self
       def async_import!(merchant_id, file_path)
-        Resque.enqueue(::Vendor::Pricelist, merchant_id, file_path)
+        Resque.enqueue(Vendor::Pricelist, merchant_id, file_path)
       end
 
       def perform(merchant_id, file_path)
-        ::Vendor::Pricelist.new(merchant_id, file_path).import!
+        Vendor::Pricelist.new(merchant_id, file_path).import!
       end
     end
 
     def initialize(merchant_id, file_path)
-      notify("Ошибка загрузки файла:\n #{file_path}", true) unless File.file?(file_path)
-      @file         = File.new(file_path)
-      @merchant     = ::Vendor::Merchant.find(merchant_id)
-      @settings     = @merchant.to_activepricelist
-      @parser_class = @merchant.parser_class.presence || 'Default'
+      @file_path      = file_path
+      @merchant       = Vendor::Merchant.find(merchant_id)
+      @settings       = @merchant.to_activepricelist
+      @parser_class   = @merchant.parser_class.presence || 'Default'
+      @csv_path       = Rails.root.join('tmp',"products_to_create_#{merchant_id}.csv")
+      @update_sql     = []
+      @to_destroy     = []
+      @to_deactivate  = []
     end
 
     def import!
-      notify('Загружается прайслист')
-      upload_pricelist
-      notify('Прайслист обрабатывается')
-      parse_pricelist
-      notify('Удаляются товары, которых нет в новом прайслисте')
-      # @products_names    = @merchant.products.pluck('vendor_products.name')
-      @products_articuls = @merchant.products.pluck('vendor_products.articul')
-      delete_not_in_pricelist
-      batch_create_or_update
-      # notify('Привязываются товары')
-      # Binder.perform
+      @init_time = Time.now
+      process_new_pricelist
+      group_products_articuls
+      compose_update_sql
+      compose_create_csv
+      write_changes_to_db
       notify('Пересчитывается цена товаров')
       @merchant.catalog_products.recount
       notify
       true
-    rescue Exception => e
+    rescue => e
       err = %(Произошла ошибка (#{e.class}): #{e.message}\n#{e.backtrace.first(5).join("\n")})
       notify(err, true)
       false
     ensure
+      FileUtils.rm @csv_path if File.file? @csv_path
       FileUtils.rm @merchant.pricelist_path if File.file? @merchant.pricelist_path
     end
 
   private
 
-    def notify(message = nil, error = false)
-      if message.present?
-        @merchant.update(pricelist_state: message, pricelist_error: error.to_s)
+    def process_new_pricelist
+      notify('Прайслист обрабатывается')
+      if File.file?(@file_path)
+        @file = File.new(@file_path)
+        File.open(@merchant.pricelist_path, 'wb'){ |f| f.puts @file.read }
       else
-        @merchant.update(pricelist_state: nil, pricelist_error: false, last_price_date: Time.now.strftime("%e.%m %X") )
+        notify("Ошибка загрузки файла:\n #{@file_path}", true)
+        return
       end
+      pricelist = "#{@parser_class}Parser".constantize.new(@settings).perform
+      @products = pricelist.products
     end
 
-    def delete_not_in_pricelist
-      @to_delete = @products_articuls - @products.map{ |p| p['articul'] }
-      @merchant
-        .products
-        .unbound
-        .where(vendor_products: { articul: @to_delete })
-        .delete_all
-      @merchant
-        .products
-        .bound
-        .where(vendor_products: { articul: @to_delete })
-        .deactivate
+    def group_products_articuls
+      notify('Новый прайслист сравнивается со старым')
+      @existing_products = @merchant.products.select(:articul, :catalog_product_id)
+      @existing_articuls = @existing_products.map { |p| p['articul'] }
+      @new_articuls = @products.map { |p| p['articul'] }
+      @existing_products.each do |p|
+        next if p['articul'].in?(@new_articuls)
+        if p['catalog_product_id'].nil?
+          @to_destroy << p['articul']
+        else
+          @to_deactivate << p['articul']
+        end
+      end
+      @to_update_articuls = @new_articuls & @existing_articuls
+      @to_create_articuls = @new_articuls - @to_update_articuls
     end
 
-    def upload_pricelist
-      File.open(@merchant.pricelist_path, 'wb'){ |f| f.puts @file.read }
-    end
-
-    def parse_pricelist
-      @pricelist ||= "#{@parser_class}Parser".constantize.new(@settings).perform
-      @products = @pricelist.products
-      @pricelist
-    end
-
-    def batch_create_or_update
+    def compose_update_sql
       notify('Находятся прайсы, которые нужно обновить')
-      from_hash_to_update
-      notify('Находятся прайсы, которые нужно создать')
-      from_hash_to_create
-      notify('Обновляются существующие прайсы')
-      batch_update( @to_update )
-      notify('Создаются новые прайсы')
-      batch_create( @to_create )
-      # actual_products = @to_create_articuls.try(:count).to_i + @to_update_articuls.try(:count).to_i
-      notify('Товары в текущем прайс листе помечаются актуальными')
-      actual_articuls = @products.map{ |p| p['articul'] }
-      Vendor::Product.where(articul: actual_articuls).activate
-    end
-
-    def batch_update( products )
-      sql = products.compact.map do |p|
+      @merchant.products.where(vendor_products: { articul: @to_update_articuls }).each do |p|
         info_attrs = p.info.to_json
-        expression = %(
+        @update_sql << %(
           UPDATE "vendor_products" SET
             "price" = #{p['price']},
             "is_rrc" = #{p['is_rrc']},
             "in_stock" = #{p['in_stock']},
             "info" = $json$#{info_attrs}$json$
-          WHERE "articul" = '#{p['articul']}'
-            AND "vendor_merchant_id" = '#{@merchant.id}';)
-      end.join
-      ActiveRecord::Base.connection.execute(sql)
+          WHERE "articul" = '#{p['articul']}' AND "vendor_merchant_id" = '#{@merchant.id}';)
+      end
     end
 
-    def batch_create( products )
-      @to_create_articuls = products.map(&:id)
-      return if products.empty?
-      csv_path = Rails.root.join('tmp','products_to_create.csv')
-      ::CSV.open(csv_path, "wb") do |csv|
-        products.each do |product|
-          csv << product.to_csv
+    def compose_create_csv
+      notify('Находятся прайсы, которые нужно создать')
+      CSV.open(@csv_path, "wb") do |csv|
+        @products.select { |p| p['articul'].in?(@to_create_articuls) }.each do |p|
+          attrs = p.slice( *REAL_ATTRIBUTES )
+          attrs['vendor_merchant_id'] = @merchant.id
+          csv << Vendor::Product.new(attrs).to_csv
         end
       end
-      begin
-        ActiveRecord::Base.connection.execute("COPY vendor_products(#{CSV_COLUMNS}) FROM '#{csv_path}' WITH(FORMAT csv);")
-      ensure
-        FileUtils.rm csv_path
+    end
+
+    def write_changes_to_db
+      notify('Применяются изменения')
+      ActiveRecord::Base.transaction do
+        ActiveRecord::Base.connection.execute("COPY vendor_products(#{CSV_COLUMNS}) FROM '#{@csv_path}' WITH(FORMAT csv);")
+        ActiveRecord::Base.connection.execute(@update_sql.join("\n"))
+        @merchant.products.where(vendor_products: { articul: @to_destroy }).delete_all
+        @merchant.products.where(vendor_products: { articul: @to_deactivate }).deactivate
+        @merchant.products.where(vendor_products: { articul: @new_articuls }).activate
       end
     end
 
-    def from_hash_to_update
-      articuls             = @products.map{ |p| p['articul'] }
-      @to_update           = @merchant.products.where(articul: articuls).all
-      @to_update_articuls  = @to_update.pluck(:articul)
-      @to_update = @to_update.map do |product|
-        new_product = @products.detect{ |p| p['articul'] == product.articul }
-        next if !product_changed?(product, new_product)
-        new_product_attributes = new_product.slice( *UPDATE_ATTRIBUTES )
-        new_product_attributes.each do |k, v|
-          product.send("#{k}=", v)
-        end
-
-        product
+    def notify(message = nil, error = false)
+      t = Time.now.to_i - @init_time.to_i
+      if message.present?
+        @merchant.update(pricelist_state: "[#{t} с]#{message}", pricelist_error: error.to_s)
+      else
+        @merchant.update(pricelist_state: nil, pricelist_error: false, last_price_date: Time.now.strftime("%e.%m %X") )
       end
     end
-
-    def from_hash_to_create
-      @to_create = @products.reject{ |p| p['articul'].in?( @to_update_articuls ) }
-      @to_create.each{ |p| p['vendor_merchant_id'] = @merchant.id  }
-      @to_create.map!{ |p| ::Vendor::Product.new(p.slice( *REAL_ATTRIBUTES )) }
-      # @TODO manual validations
-      # @to_create.uniq!{ |p| p.name }
-      @to_create.uniq!{ |p| p.articul }
-      @to_create.reject!{ |p| p.articul.blank? || p.articul.in?(@products_articuls) } # or p.name.in?(@products_names)
-    end
-
-    def product_changed?(old_product, new_product)
-      old_product.price != new_product['price'] ||
-      old_product.in_stock != new_product['in_stock'] ||
-      old_product.in_stock_kiev != new_product['in_stock_kiev'] ||
-      old_product.in_stock_kharkov != new_product['in_stock_kharkov'] ||
-      old_product.is_rrc != new_product['is_rrc'] ||
-      old_product.rrc != new_product['rrc'] ||
-      old_product.uah != new_product['uah'] ||
-      old_product.usd != new_product['usd'] ||
-      old_product.eur != new_product['uer']
-    end
-
   end
 end
